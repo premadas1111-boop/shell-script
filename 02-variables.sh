e3bb8b9b69908266394d578c770bc3a39e304735
premadas <premadas1111@gmail.com>
2026-06-16 17:49:08 +0530

    03-variables.sh

--- 02-variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "ravi:: hai prem how are you?"
-echo "prem:: hai ravi i am fine how are you?"
-echo "ravi:: i am fine prem what are u doing?"
-echo "prem:: ha ravi iam doing business and you?"
