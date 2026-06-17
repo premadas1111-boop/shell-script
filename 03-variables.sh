70c6471d7a408d61bd4ee1dcb85252fea553074f
premadas <premadas1111@gmail.com>
2026-06-17 13:25:04 +0530

    shell scprit added

--- 03-variables.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PERSON1=RAMESH
-PERSON2=SURESH
-
-echo "$PERSON1:: hai $PERSON2 how are you?"
-echo "$PERSON2:: hai $PERSON1 ravi i am fine how are you?"
-echo "$PERSON1:: i am fine $PERSON2 what are u doing?"
-echo "$PERSON2:: ha $PERSON1 iam doing business and you
--- 05-variables.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "prem:: hai naveen"
-echo "naveen:: hello prem"
-echo "prem:: fine what are u doing?"
